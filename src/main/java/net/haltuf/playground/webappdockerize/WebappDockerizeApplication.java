89386f568c74641dfdb1f56f4e9150825010fb06
Ludvik Haltuf <ludvik.haltuf@gmail.com>
2023-07-02 20:50:15 +0200

    created skeleton

---
+++ src/main/java/net/haltuf/playground/webappdockerize/WebappDockerizeApplication.java
@@ -0,0 +1,13 @@
+package net.haltuf.playground.webappdockerize;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebappDockerizeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebappDockerizeApplication.class, args);
+    }
+
+}
